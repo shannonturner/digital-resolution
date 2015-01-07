fc62433adcddfe540ecb08c69fe2fdf14d0c3656
Shannon Turner <shannon.v.turner@gmail.com>
2015-01-07 18:24:24 -0500

    Design improvements, now refreshes page every 30s, shows logo instead of resolution 15% of time, applies random bgcolor

--- urls.py
+++ urls.py
@@ -8,6 +8,6 @@ urlpatterns = patterns('',
     # url(r'^$', '.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^(?:resolutions/)?admin/', include(admin.site.urls)),
     url(r'^$', ResolutionView.as_view()),
 )
